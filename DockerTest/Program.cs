543426a643ffd3870fb6b5bc2623538f454e297e
Moe Farag <fozdoq@gmail.com>
2016-03-11 00:56:19 +0200

    initial commit

---
+++ DockerTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DockerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timer = new Timer(CallBackMethod, null, 0, 2000);
+
+            Console.Read();
+        }
+
+        private static void CallBackMethod(object state)
+        {
+            Console.WriteLine(DateTime.UtcNow.ToString("yyyy-MM-dd hh:mm:ss"));
+        }
+    }
+}
